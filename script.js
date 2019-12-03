04ba77f629bf361f267e4c14462ec3dde6f058c3
TuyenLe <congtuyen598@gmail.com>
2019-12-03 22:05:02 +0700

    add recording feature

--- script.js
+++ script.js
@@ -10,17 +10,55 @@ const validButtons = [
 ];
 
 let activeButtons = [];
+let musicSheets = [];
+let recording = false;
 
 function onButtonPress(e) {
-  console.log(e.code);
-  if (!validButtons.includes(e.code)) { return; }
+  if (!validButtons.includes(e.code) || activeButtons.includes(e.code)) { return; }
+  activeButtons.push(e.code);
+  console.log('ping', activeButtons);
   document.querySelector(`#${e.code}`).classList.add('pressed');
+  if (recording) {
+    musicSheets.push(e.code);
+  }
 }
 
 function onButtonUp(e) {
-  console.log(`Button ${e.code} upped`);
   if (!validButtons.includes(e.code)) { return; }
+  activeButtons = activeButtons.filter(btn => btn != e.code);
+  // gui ki hieu tat nut (low) tuong ung toi ESP8266
+  // ... vidu: 'lKeyA', 'lKeyS', ...
+  console.log('low', `l${e.code}`);
   document.querySelector(`#${e.code}`).classList.remove('pressed');
+  if (recording) {
+    musicSheets.push(`l${e.code}`); // push in the stop point to music sheet
+  }
+}
+
+function changeRecordStatus() {
+  recording = !recording;
+  if (recording) {
+    musicSheets = []; // khoi tao lai ban ghi am
+    document.querySelector('#record-btn').textContent = 'Dừng ghi âm...';
+    let playBtn = document.querySelector('.play-btn');
+    document.querySelector('#record').removeChild(playBtn);
+  }
+  else {
+    document.querySelector('#record-btn').textContent = 'Ghi âm';
+    let playButton = document.createElement('button');
+    let text = document.createTextNode('Phát lại bản ghi');
+    playButton.appendChild(text);
+    playButton.classList.add('play-btn');
+    playButton.setAttribute('onclick', 'playRecord()');
+    document.querySelector('#record').appendChild(playButton);
+  }
+
+  console.log('record', recording);
+}
+
+function playRecord() {
+  console.log('playing record...', musicSheets);
+  // do something
 }
 
 document.addEventListener('keydown', onButtonPress);
